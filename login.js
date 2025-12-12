bb7ddd2eb9e9f4104b8a8bbfa9b371c220b83cf8
Vamshi Krishna <pariveda.vs@gmail.com>
2025-12-12 10:01:00 +0530

    Added login page

---
+++ login.js
@@ -0,0 +1,13 @@
+const login = () => {
+return (
+    <>
+    <h2>Login Page</h2>
+    <label>UserName: </label>
+    <input type="text" name="username" /><br /><br />
+    <label>Password: </label>
+    <input type="password" name="password" /><br /><br />
+    <button>Login</button>
+    </>
+)
+}
+export default login;
